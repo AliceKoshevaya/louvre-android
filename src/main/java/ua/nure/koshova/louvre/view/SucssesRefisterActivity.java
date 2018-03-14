9296f9ad00a3eb31efc34d5429d0da24997518c3
Alice Koshova <alisson.koshova@gmail.com>
2018-03-14 15:14:17 +0200

    Successful registration activity

---
+++ src/main/java/ua/nure/koshova/louvre/view/SucssesRefisterActivity.java
@@ -0,0 +1,15 @@
+package ua.nure.koshova.louvre.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ua.nure.koshova.louvre.R;
+
+public class SucssesRefisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucsses_refister);
+    }
+}
