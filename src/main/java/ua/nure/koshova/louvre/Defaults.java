d422a84ef3451aedd596a22b00670edca086a330
Alice Koshova <alisson.koshova@gmail.com>
2018-03-14 15:15:45 +0200

    Defaults

---
+++ src/main/java/ua/nure/koshova/louvre/Defaults.java
@@ -0,0 +1,10 @@
+package ua.nure.koshova.louvre;
+
+public class Defaults {
+
+    public static final String APPLICATION_ID = "C2102947-C0CA-1BED-FF73-CC570E9E2800";
+    public static final String API_KEY = "6A819DBE-B7EC-FB5E-FFCB-1D8A06D60200";
+    public static final String SERVER_URL = "https://api.backendless.com";
+
+}
+                                            
