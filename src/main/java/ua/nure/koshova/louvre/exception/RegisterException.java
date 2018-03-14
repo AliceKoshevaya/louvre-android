c376734730ce63b9c0b4c96a829b9815a977515a
Alice Koshova <alisson.koshova@gmail.com>
2018-03-14 14:59:30 +0200

    Create a RegisterException

---
+++ src/main/java/ua/nure/koshova/louvre/exception/RegisterException.java
@@ -0,0 +1,12 @@
+package ua.nure.koshova.louvre.exception;
+
+
+/**
+ * @author Alexander Shylin
+ */
+
+public class RegisterException extends Exception {
+    public RegisterException (String message){
+        super(message);
+    }
+}
